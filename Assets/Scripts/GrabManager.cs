24d9505f0e2e4b80a7f54d25df9e3234b183a170
Anna Ra <anna.ra.phoenix@gmail.com>
2025-02-13 01:19:39 +0300

    throw item with force

--- Assets/Scripts/GrabManager.cs
+++ Assets/Scripts/GrabManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrabManager : MonoBehaviour, IManager
 {
     public ManagerStatus Status { get; private set; }
     [SerializeField] private Transform grabOrigin;
+    [SerializeField] private Button dropBtn;
+
+    [SerializeField] private float dropForce = 15;
 
     private Draggable _draggable;
     private CharacterController _player;
@@ -12,20 +16,20 @@ public class GrabManager : MonoBehaviour, IManager
     public IEnumerator Startup(CharacterController player)
     {
         _player = player;
+        dropBtn.onClick.AddListener(Drop);
         Status = ManagerStatus.Started;
         yield return null;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
         {
             var hits = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition));
             foreach (var hit in hits)
             {
                 var item = hit.collider.gameObject.GetComponent<Draggable>();
-                if (item != null && item == _draggable) Drop();
-                else if (item) Pick(item);
+                if (item && _draggable == null) Pick(item);
             }
         }
         if (_draggable)
@@ -36,11 +40,14 @@ public class GrabManager : MonoBehaviour, IManager
     {
         _draggable = item;
         _draggable.OnPick();
+        dropBtn.gameObject.SetActive(true);
     }
 
     void Drop()
     {
         _draggable.OnDrop();
+        _draggable.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * dropForce;
         _draggable = null;
+        dropBtn.gameObject.SetActive(false);
     }
 }
